891834687155228c7811dd0008c720c8d7103ef2
Vzina <891718689@qq.com>
2016-01-27 15:30:14 +0800

    调整目录结构

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 isset($_GET['_debug']) && define('IS_DEBUG', true);
 define('ROOT_PATH', dirname(__DIR__) . '/');           /* 根目录 */
 define('APP_PATH', ROOT_PATH . 'app/');          /* 应用目录 */
-define('SYS_CONFIG_PATH', ROOT_PATH . 'config/');          /* 系统配置目录 */
+define('CONFIG_PATH', APP_PATH . 'config/');          /* 系统配置目录 */
 
-$application = new \Yaf\Application(SYS_CONFIG_PATH . "application.ini", \Yaf\ENVIRON);
+$application = new \Yaf\Application(CONFIG_PATH . "application.ini", \Yaf\ENVIRON);
 
 $application->bootstrap();
 
